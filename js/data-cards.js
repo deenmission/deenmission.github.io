e3d0ae8c06cf54c3e36aa1557272461149a5e5ed
DEEN <deenmission@gmail.com>
2025-07-19 19:24:27 +0600

    Create data-cards.js

---
+++ js/data-cards.js
@@ -0,0 +1,14 @@
+// cards-data.js
+const cards = [
+  {
+    title: "সালাতের গুরুত্ব",
+    details: "সালাত বা নামাজ ইসলামের গুরুত্বপূর্ণ স্তম্ভ...",
+    link: "pages/salat.html"
+  },
+  {
+    title: "রমজানের গুরুত্ব",
+    details: "রমজান আত্মশুদ্ধির মাস...",
+    link: "pages/siam.html"
+  }
+  // আরও কার্ড
+];
