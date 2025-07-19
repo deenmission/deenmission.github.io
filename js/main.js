033dfc72ef8220523b1a537445683e304b60c31f
DEEN <deenmission@gmail.com>
2025-07-19 21:38:32 +0600

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import { cards } from './data-cards.js';
+import { generateCards } from './generate-cards.js';
+import { initSearch } from './search.js';
+import { initMenu } from './menu.js';
+import { initIntersectionObserver } from './observer.js';
+
+const searchInput = document.getElementById("searchInput");
+const cardContainer = document.getElementById("cardContainer");
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.getElementById('mainMenu');
+const closeBtn = document.getElementById('closeMenuBtn');
+const noResultsCard = document.getElementById("noResultsCard");
+
+generateCards(cards, cardContainer);
+initIntersectionObserver();
+initSearch(searchInput, cardContainer, noResultsCard);
+initMenu(menuIcon, menu, closeBtn);
